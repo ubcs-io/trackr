d3aeffca4081352b5f6a62af657efd334d4c639a
Roman <roman@ubcs.io>
2017-12-28 00:12:46 -0500

    Change default value to 5% for scalar widgets

--- html/view/dashboard/widgets/scalar.php
+++ html/view/dashboard/widgets/scalar.php
@@ -11,12 +11,11 @@
 		// For each of those modules that is active, call the widget view
 		foreach ($db->scalar_widgets as $widget) { ?>
 
-			<div class="col-xs-6 col-md-6 well">
+			<div class="col-xs-6 col-md-6">
 
 				<div class="lead"><?php echo $widget['name']; ?> <br></div>
 					<div class="progress">
-					  <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width: 45%">
-					    <span class="sr-only">45% Complete</span>
+					  <div class="progress-bar progress-bar-danger progress-bar-striped active" role="progressbar" aria-valuenow="5" aria-valuemin="0" aria-valuemax="100" style="width: 5%">
 					  </div>
 					</div>
 
