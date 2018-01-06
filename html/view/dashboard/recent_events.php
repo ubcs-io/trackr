115484aa0109db99572b001acb736fb947de0a5f
Roman <roman@ubcs.io>
2018-01-06 16:58:59 -0500

    Add the view for recent events on the dashboard

---
+++ html/view/dashboard/recent_events.php
@@ -0,0 +1,39 @@
+
+<?php
+
+?>
+
+  <h2 class="sub-header">Recent Events</h2>
+  <div class="table-responsive">
+   
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th>Event Name</th>
+          <th>Date</th>
+          <th>Value</th>
+        </tr>
+      </thead>
+      <tbody>
+
+	<?php
+
+		// For each of those modules that is active, call the widget view
+
+		foreach ($recent_events->events as $event) { 
+			#var_dump($event);
+			?>
+			    <tr>
+                  <td><?php echo $event["table_name"]; ?></td>
+                  <td><?php echo $event["date"]; ?></td>
+                  <td><?php echo $event["value"]; ?></td>
+                </tr>
+			
+		<?php }
+
+	?>
+
+      </tbody>
+    </table>
+
+  </div>
