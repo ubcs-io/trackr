e02fc4e94e5d3605973ff5a33fcc7652b784a2fd
Roman <roman@ubcs.io>
2018-01-07 20:55:54 -0500

    Add example authorization file

--- html/controller/authorization.php
+++ html/controller/authorization.php
@@ -2,7 +2,22 @@
 
 // Verify user is supposed to be permitted. Left generic for end users.
 
-if (1) {
-  // User has permission
+if (isset($_COOKIE['trackr']) && $_COOKIE['trackr'] == "Authorized") {
+
+  // User has permission already
+
+} elseif (isset($_GET['user']) && $_GET['pass'] == "userpassword") {
+
+	// Set a cookie with the value "Authorized"
+	setcookie('trackr', 'Authorized', time() + (365 * 24 * 60), "/" );
+
+} else {
+
+	# The block below is commented out since each authorization file should be different
+	# Please do not use this default layout in a live project
+
+	// Otherwise, send them someplace else
+	// header ("Location: http://lmgtfy.com/");
+	// exit;
 
 }
