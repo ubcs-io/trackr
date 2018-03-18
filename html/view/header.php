e76512f70a6045818f4c6553d5b3989bf472cafd
Roman <roman@ubcs.io>
2018-03-18 10:18:23 -0400

    Add a link to the overview page for better movement on mobile without the left sidebar

--- html/view/header.php
+++ html/view/header.php
@@ -35,14 +35,15 @@
 
           <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav navbar-right">
+              <li><a href="/">Overview</a></li>
               <li><a href="log.php">Log</a></li>
             </ul>
 
-            <ul class="nav navbar-nav navbar-right">
+<!--             <ul class="nav navbar-nav navbar-right">
               <li><a href="?range=7">Week</a></li>
               <li><a href="?range=30">Month</a></li>
               <li><a href="?range=90">Quarter</a></li>
-            </ul>
+            </ul> -->
 
         </div>
       </div>
